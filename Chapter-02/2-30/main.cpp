d980e20b230fb65d514e4e1a91494f097e780a9e
expoli <31023767+expoli@users.noreply.github.com>
2021-10-25 16:30:06 +0800

    2-32 添加减法测试函数

--- Chapter-02/2-30/main.cpp
+++ Chapter-02/2-30/main.cpp
@@ -8,10 +8,19 @@
  * @param t 第二个参数
  * @return 测试结果
  */
-int tadd_ok(int x, int t);
+int tadd_ok(int x, int y);
+
+/**
+ * 测试两个数是否能顺利的进行减
+ * @param x 第一个数
+ * @param y 第二数
+ * @return 测试结果
+ */
+int tsub_ok(int x, int y);
 
 int main() {
     std::cout << tadd_ok(INT32_MIN, 1) << std::endl;
+    std::cout << tsub_ok(INT32_MIN, 1) << std::endl;
     return 0;
 }
 
@@ -29,4 +38,8 @@ int tadd_ok(int x, int y) {
             return true;
     }
     return true;
+}
+
+int tsub_ok(int x, int y) {
+    return tadd_ok(x, -y);
 }
