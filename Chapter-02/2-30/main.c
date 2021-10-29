1de3845c71ee54a32e79ccd5d4c37b1c0a6df0f5
expoli <31023767+expoli@users.noreply.github.com>
2021-10-29 20:50:59 +0800

    1. [fix] 修复原加法测试函数等于时的处理

--- Chapter-02/2-30/main.c
+++ Chapter-02/2-30/main.c
@@ -11,7 +11,7 @@ int tadd_ok(int x, int y) {
     int s = x + y;
     if (x < 0 && y < 0 && s >= 0)
         return 0;
-    if (x > 0 && y > 0 && s < 0)
+    if (x > 0 && y > 0 && s <= 0)
         return 0;
     return 1;
 }
