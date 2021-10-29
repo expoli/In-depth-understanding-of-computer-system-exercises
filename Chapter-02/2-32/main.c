96c3e6a6434bfee035c0102c3c3a85133719896c
expoli <31023767+expoli@users.noreply.github.com>
2021-10-29 20:51:25 +0800

    减法测试暂时没有找到bug值

---
+++ Chapter-02/2-32/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "limits.h"
+
+/**
+ * Determine whether arguments can be added without overflow
+ * @param x
+ * @param y
+ * @return
+ */
+int tadd_ok(int x, int y) {
+    int s = x + y;
+    if (x < 0 && y < 0 && s >= 0)
+        return 0;
+    if (x > 0 && y > 0 && s <= 0)
+        return 0;
+    return 1;
+}
+
+/**
+ * Determine whether arguments can be subtracted without overflow
+ * WARNING: This code is buggy
+ * @param x
+ * @param y
+ * @return
+ */
+int tsub_ok(int x, int y) {
+    return tadd_ok(x, -y);
+}
+
+/**
+ * I can't found out what value will make mistake.
+ * @return
+ */
+int main() {
+    printf("%d\n", tsub_ok(INT_MIN, 1));
+    printf("%d\n", tsub_ok(INT_MAX, -1));
+    return 0;
+}
